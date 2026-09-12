73d9518e7cbc06cc195ecd27f77486a3b48bc73a
khgoh2025 <kh.goh.2025@smu.edu.sg>
2026-09-12 22:00:00 +0800

    Ex3

--- src/components/Ex3.vue
+++ src/components/Ex3.vue
@@ -8,8 +8,24 @@ const selectedOp = ref("+")
 
 // TODO: Add Code Here
 // Note: DO NOT USE "eval()". In security, "eval" is considered "evil"!!!
+const result = computed(() => {
+    if (selectedOp.value === "+"){
+        return x.value + y.value
+    }
+    else if (selectedOp.value === "-"){
+        return x.value - y.value
+    }
+    else if (selectedOp.value === "*"){
+        return x.value * y.value
+    }
+    else if (selectedOp.value === "/"){
+        return x.value / y.value
+    }
+    else {
+        return x.value % y.value
+    }
+})
 
-    
 </script>
 
 <template>
