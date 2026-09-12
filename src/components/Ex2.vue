08e4c1533590d9802ff91d480abf9fe6cd68353e
khgoh2025 <kh.goh.2025@smu.edu.sg>
2026-09-12 19:00:33 +0800

    Exercise 2 Scoreboard

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -33,17 +33,22 @@ function reset() {
         <p>Current: {{ scoreA }} - {{ scoreB }}</p>
 
         <!-- B. In-template expressions go here -->
+        <p>Total Points: {{ scoreA  +  scoreB }}</p>
+        <p>Points Left to Win: {{ 10 - (scoreA > scoreB ? scoreA : scoreB) }}</p> <!--Can use Math.max(scoreA, scoreB)-->
 
         <!-- A. Event handlers go here -->
         <div style="display: flex; gap: 12px; margin: 12px 0;">
-            <button>+ Team A</button>
-            <button>+ Team B</button>
-            <button>Reset</button>
+            <button v-on:click="addA">+ Team A</button>
+            <button v-on:click="addB">+ Team B</button>
+            <button v-on:click="reset">Reset</button>
         </div>
 
 
         <div style="margin-top: 14px;">
             <!-- C. Display winner / status here -->
+        <p v-if="scoreA === maxScore">Winner: {{ teamA.toUpperCase() }}</p>
+        <p v-else-if="scoreB === maxScore">Winner: {{ teamB.toUpperCase() }}</p>
+        <p v-else>No winner yet. Keep playing!</p>
         </div>
 
 
