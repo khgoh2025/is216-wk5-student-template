1b333e419b976d0e614a542b6e1a4ce7c2e160fe
khgoh2025 <kh.goh.2025@smu.edu.sg>
2026-09-14 15:14:30 +0800

    Exercise 4

--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -3,7 +3,24 @@ import { ref } from 'vue'
 const items = ref(['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'])
 
 // TODO: Add Code Here
+function deleteItem(item){
+    let idx = -1
+    for (const stuff of items.value){
+        if (stuff.trim() === item.trim()){
+            idx = items.value.indexOf(stuff)
+        }
+    }
+    items.value.splice(idx, 1)
+}
 
+const chosenItem = ref("")
+
+function addItem(){
+    if (chosenItem.value.trim() !== ""){
+        items.value.push(chosenItem.value)
+        chosenItem.value = ""
+    }
+}
 
 </script>
 
@@ -12,5 +29,11 @@ const items = ref(['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'])
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
 
-   
+    <ul v-for="item in items" v-bind:key="item">
+        <li>{{ item }}
+            <button v-on:click="deleteItem(item)">Delete!</button>
+        </li>
+    </ul>
+    <input v-model="chosenItem" type="text">
+    <button v-on:click="addItem(chosenItem)">Add!</button>
 </template>
