27b86f87a2a3db898d4b52a487fed4b9158b637b
khgoh2025 <kh.goh.2025@smu.edu.sg>
2026-09-15 18:38:24 +0800

    Ex5 changes

--- src/components/Ex5.vue
+++ src/components/Ex5.vue
@@ -5,10 +5,22 @@ const moods = ref(['Happy', 'Sad', 'Angry']);
 const subject = ref('');
 const entry = ref('');
 const mood = ref('');
+const url = "http://localhost:8000/posts"
 
 // Add Code Here
 
-
+async function addPost(){
+    try {
+        const response = await axios.post(url, 
+            {subject: subject.value,
+            entry: entry.value,
+            mood: mood.value
+            }
+        )
+    } catch (error) {
+        console.log(error.message)
+    }
+}
 </script>
 
 <template>
@@ -24,14 +36,17 @@ const mood = ref('');
 
         Mood:
         <!-- TODO: Build a dropdown list here for selecting the mood -->
+        <select v-model="mood">
+            <option v-for="emotion in moods">{{ emotion }}</option>
+        </select>
         <br>
 
         <br>
-        <button>Submit New Post</button>
+        <button @click="addPost">Submit New Post</button>
 
         <hr>
         <RouterLink to="/ViewPosts/">Click  here to return to Main Page</RouterLink>  
-       
+    
     </div>
 </template>
 
